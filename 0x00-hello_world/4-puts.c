e68cf35c358bb7a2407c95d794b419478bd61830
Noah Sichone <sichonenoah@gmail.com>
2022-09-11 07:17:16 -0700

    printing with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  char words[] = "\"Programming is like building a multilingual puzzle";
+  puts(words);
+  return (0);
+}
