ae23ab849bc1f49ee0c8634405b8411681bcf3da
Noah Sichone <sichonenoah@gmail.com>
2022-09-11 13:25:21 -0700

    quit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,17 +20,17 @@ n = rand() - RAND_MAX / 2;
 
 if (n > 0)
 {
-  printf("%d is positive\n", n);
+ printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
-  printf("%d is zero\n", n);
+ printf("%d is zero\n", n);
 }
  else if (n < 0)
 {
-  printf("%d is negative\n", n);
+ printf("%d is negative\n", n);
 }
 else
-  printf("Invalid number entered\n");
+ printf("Invalid number entered\n");
 return (0);
 }
