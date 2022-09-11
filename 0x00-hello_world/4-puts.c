f2be97e0615e931906173506a29ce7d6b979fe36
Noah Sichone <sichonenoah@gmail.com>
2022-09-11 07:23:10 -0700

    1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-
 /**
  *main - Entry point
  *
  *Return: Always 0 (Success)
  */
-
 int main(void)
 {
-  char words[] = "\"Programming is like building a multilingual puzzle";
-  puts(words);
-  return (0);
+ char words[] = "\"Programming is like building a multilingual puzzle";
+ puts(words);
+ return (0);
 }
