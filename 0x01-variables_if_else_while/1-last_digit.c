79873ee80128a11f9f1d8a6ad30ab58802bc8eb9
Noah Sichone <sichonenoah@gmail.com>
2022-09-11 13:35:08 -0700

    quit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,6 +9,7 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 int n, x;
@@ -31,3 +32,4 @@ printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
 else
 printf("Invalid number");
 return (0);
+}
