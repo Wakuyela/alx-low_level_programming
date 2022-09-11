ac28e0f9a22fce16533714b31f882e678f3b86c6
Noah Sichone <sichonenoah@gmail.com>
2022-09-11 07:26:38 -0700

    1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
 */
 int main(void)
 {
- char words[] = "\"Programming is like building a multilingual puzzle";
- puts(words);
- return (0);
+char words[] = "\"Programming is like building a multilingual puzzle";
+puts(words);
+return (0);
 }
