3c9d959b97fb0f84fad6af5826bdabd3ffe23e81
Noah Sichone <sichonenoah@gmail.com>
2022-09-13 11:57:36 -0700

    main and header

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
- * main - prints putchar
- *
- * return - always 0
- */
+* main - prints putchar
+*
+* return - always 0
+*/
 
 int main(void)
 {
-  _putchar('_');
-  _putchar('p');
-  _putchar('u');
-  _putchar('t');
-  _putchar('c');
-  _putchar('h');
-  _putchar('a');
-  _putchar('r');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 
-  return (0);
+return (0);
 }
