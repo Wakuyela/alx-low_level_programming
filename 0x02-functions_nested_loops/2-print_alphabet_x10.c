dbd90776d15fd5c7f5eba5126c6629deb8879387
Noah Sichone <sichonenoah@gmail.com>
2022-09-13 13:04:04 -0700

    alphax10x3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,16 +4,15 @@
 *
 * Return: 0
 */
-
 void print_alphabet_x10(void)
 {
 char a, b;
-for( a = 0; a <= 9; a++)
+for ( a = 0; a <= 9; a++)
 {
 for (b = 'a'; b <= 'z'; b++)
 {
 _putchar(b);
 }
- _putchar('\n');
+_putchar('\n');
 }
 }
