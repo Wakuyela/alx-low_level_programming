001dfe1b16dd6117e10718167991e1c2b8033118
Noah Sichone <sichonenoah@gmail.com>
2022-09-13 12:21:29 -0700

    voided

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* main - prints alphabet
+*
+* Return: always 0
+*/
+
+void print_void(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('n');
+}
