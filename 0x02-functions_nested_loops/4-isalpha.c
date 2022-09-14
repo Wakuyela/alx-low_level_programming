5ec16b4a56eefbc0c5f4abcc21050ec77fb56a3e
Noah Sichone <sichonenoah@gmail.com>
2022-09-13 23:42:33 -0700

    nor

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 /**
 * _isalpha - function checks for upcase characters
 * @c: is the int being used for arguments for the function
-* Return: Always 0
+* Return: Always (0)
 */
 int _isalpha(int c)
 {
