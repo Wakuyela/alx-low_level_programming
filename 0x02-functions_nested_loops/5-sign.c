12e9e7b2d8ad69b6513bab11cb07eb81b57dcb06
Noah Sichone <sichonenoah@gmail.com>
2022-09-13 23:58:29 -0700

    interger values

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - function checks for value of integer
+ *@n: is the int value being checked
+ * Return: 0
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+else
+{
+_putchar('0');
+return (0);
+}
+}
