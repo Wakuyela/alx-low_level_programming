5f2f860cc61cf1b8d20105ea07d9e0d1e8e8312b
Noah Sichone <sichonenoah@gmail.com>
2022-09-14 01:18:24 -0700

    Jackx3

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
-* void jack_bauer(void)- function prints every minute of the day of Jack Bauer, starting from 00:00
-* to 23:59
+*jack_bauer- function prints every minute of the day of Jack Bauer,
+*starting from 00:00 to 23:59
+*
 *Return: 0
 */
 void jack_bauer(void)
